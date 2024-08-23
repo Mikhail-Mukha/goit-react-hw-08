89efc8790ef67ae8750e186b596043cda8015007
Mikhail-Mukha <misha500krut@gmail.com>
2024-08-23 21:10:45 +0300

    finish-commit

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -1,11 +1,15 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { logoutThunk } from "../../redux/auth/operations";
 import s from "./UserMenu.module.css";
+import { selectUser } from "../../redux/auth/selectors";
 
 const UserMenu = () => {
+  const { name } = useSelector(selectUser);
   const dispatch = useDispatch();
+
   return (
     <div className={s.buttonDiv}>
+      <p className={s.text}>Welcome, {name}!</p>
       <button
         className={s.button}
         onClick={() => {
